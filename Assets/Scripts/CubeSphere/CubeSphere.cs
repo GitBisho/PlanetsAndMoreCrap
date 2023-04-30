29349b19a5f31b03312fb75c09b4f1d74a513db5
GitBisho <bishopjherrington@gmail.com>
2023-04-30 01:05:37 -0500

    commit test

--- Assets/Scripts/CubeSphere/CubeSphere.cs
+++ Assets/Scripts/CubeSphere/CubeSphere.cs
@@ -7,7 +7,7 @@ public class CubeSphere : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //sdatugyxuzyuy8ikughsaixzujh
+        //sdatugyxuzyuy8ikughsaixzujhesdfredf
     }
 
     // Update is called once per frame
